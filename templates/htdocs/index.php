2ea592606fa13ce643b803ce05afd623ec8dcf63
Daniel Fernandez <daniel.fdez.fdez@gmail.com>
2015-03-14 17:10:06 +0100

    - Add TIMEZONE

--- templates/htdocs/index.php
+++ templates/htdocs/index.php
@@ -24,12 +24,12 @@
  * @author   Daniel Fernández <daniel.fdez.fdez@gmail.com>
  * @license  http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
  * @version  SVN: $Id: index.php 120 2013-07-29 08:48:14Z carlosmelga $
- * @link     http://sourceforge.net/projects/yuju/
+ * @link     https://github.com/yuju-framework/yuju
  * @since    version 1.0
  */
 session_start();
 require '../conf/site.php';
-date_default_timezone_set('Europe/Madrid');
+date_default_timezone_set(TIMEZONE);
 /**
  * Autoload classes
  *
