195af6465955bda4a61cb3e8b13e5e66f29d1a1d
Daniel Fernandez <daniel.fdez.fdez@gmail.com>
2016-01-20 20:23:52 +0100

    Fix indicate name object

--- lib/orm.php
+++ lib/orm.php
@@ -2,28 +2,10 @@
 /**
  * Yuju ORM command line File
  *
- * PHP version 5
- *
- * Copyright individual contributors as indicated by the @authors tag.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- *
  * @category CLI
  * @package  YujuFramework
  * @author   Daniel Fernández <daniel.fdez.fdez@gmail.com>
  * @license  http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
- * @version  GIT: 
  * @link     https://github.com/yuju-framework/yuju
  * @since    version 1.0
  */
@@ -38,25 +20,21 @@
  */
 function orm($directory, $command, $type, $table, $name = null)
 {
-    // TODO: read config, connect to data base and delete database
     include 'lib/config.php';
     if (substr($directory, strlen($directory) - 1) != '/') {
         $directory=$directory.'/';
     }
     include $directory.'conf/site.php';
-    //if (class_exists($type, true) && is_a($type, 'AbstractYuju_ORM')) {
-        $orm = new Yuju_ORM(new $type);
-        switch ($command) {
-            case 'object':
-                $orm->load($table);
-                $file = new File();
-                if ($name == null) {
-                    $name = ucwords($table);
-                }
-                $file->setContent($orm->generateObject($name));
-                $file->create($directory.'class/'.$name.'.php');
-                $file->close();
-        }
-    //}
-    //echo _('Type do not exist')."\n";
+    $orm = new Yuju_ORM(new $type);
+    switch ($command) {
+        case 'object':
+            $orm->load($table);
+            $file = new File();
+            if ($name == null) {
+                $name = $table;
+            }
+            $file->setContent($orm->generateObject($name));
+            $file->create($directory.'class/'.$name.'.php');
+            $file->close();
+    }
 }
--- yuju.php
+++ yuju.php
@@ -3,28 +3,10 @@
 /**
  * Yuju command line File
  *
- * PHP version 5
- *
- * Copyright individual contributors as indicated by the @authors tag.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- *
  * @category CLI
  * @package  YujuFramework
  * @author   Daniel Fernández <daniel.fdez.fdez@gmail.com>
  * @license  http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
- * @version  GIT: 
  * @link     https://github.com/yuju-framework/yuju
  * @since    version 1.0
  */
@@ -53,33 +35,32 @@ if (count($argv)<2) {
 }
 
 switch ($argv[1]) {
-case 'create':
-    include 'lib/create.php';
-    break;
-case 'delete':
-    include 'lib/delete.php';
-    delete($argv[2]);
-    break;
-case 'compile':
-    include 'lib/compile.php';
-    compile($argv[2], $argv[3]);
-    break;
-case 'orm':
-    include 'lib/orm.php';
-    if (count($argv)<5) {
+    case 'create':
+        include 'lib/create.php';
+        break;
+    case 'delete':
+        include 'lib/delete.php';
+        delete($argv[2]);
+        break;
+    case 'compile':
+        include 'lib/compile.php';
+        compile($argv[2], $argv[3]);
+        break;
+    case 'orm':
+        include 'lib/orm.php';
+        if (count($argv)<5) {
+            showHelp();
+            exit;
+        }
+        if (isset($argv[6])) {
+            $name = $argv[6];
+        } else {
+            $name = null;
+        }
+        orm($argv[2], $argv[3], $argv[4], $argv[5], $name);
+        break;
+    default:
         showHelp();
         exit;
-    }
-    if (isset($arv[6])) {
-        $name = $arv[6];
-    } else {
-        $name = null;
-    }
-    orm($argv[2], $argv[3], $argv[4], $argv[5], $name);
-    break;
-default:
-    showHelp();
-    exit;
-    break;
+        break;
 }
-?>
